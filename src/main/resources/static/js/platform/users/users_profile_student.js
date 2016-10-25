3c7b2089f60861fcee2a99ff5197f1c3fe0c52df
zbeboy <863052317@qq.com>
2016-10-25 09:58:19 +0800

    添加modal

--- src/main/resources/static/js/platform/users/users_profile_student.js
+++ src/main/resources/static/js/platform/users/users_profile_student.js
@@ -1,3 +1,12 @@
 /**
  * Created by lenovo on 2016-10-23.
  */
+requirejs.config({
+    // pathsオプションの設定。"module/name": "path"を指定します。拡張子（.js）は指定しません。
+    paths: {
+        "nav": web_path + "/js/util/nav"
+    }
+});
+require(["jquery","nav"], function ($,nav) {
+
+});
