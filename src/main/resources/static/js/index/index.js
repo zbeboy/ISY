8723c95b7e71879d7e25b8cc1e4049fd87e4f88c
zbeboy <863052317@qq.com>
2017-01-22 14:39:13 +0800

    过年庆祝烟火

--- src/main/resources/static/js/index/index.js
+++ src/main/resources/static/js/index/index.js
@@ -1,10 +1,31 @@
 /**
  * Created by lenovo on 2016-08-17.
  */
+requirejs.config({
+    // pathsオプションの設定。"module/name": "path"を指定します。拡張子（.js）は指定しません。
+    paths: {
+        "jquery.fireworks": web_path + "/plugin/jquery_fireworks/jquery.fireworks"
+    },
+    // shimオプションの設定。モジュール間の依存関係を定義します。
+    shim: {
+        "jquery.fireworks": {
+            // jQueryに依存するのでpathsで設定した"module/name"を指定します。
+            deps: ["jquery"]
+        }
+    }
+});
 // require(["module/name", ...], function(params){ ... });
-require(["jquery"], function ($) {
+require(["jquery", "jquery.fireworks"], function ($) {
     // Preloader
     $('#status').delay(300).fadeOut();
     $('#preloader').delay(300).fadeOut('slow');
     $('body').delay(550).css({'overflow': 'visible'});
+
+    // 烟火 庆祝准备
+    $('.intro-header').fireworks({
+        sound: true, // 声音效果
+        opacity: 0.5,
+        width: '100%',
+        height: '100%'
+    });
 });
