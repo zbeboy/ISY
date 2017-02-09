ab4b3d46fec0d1cd36b6079e007b78c916b5aeff
zbeboy <863052317@qq.com>
2017-02-09 21:31:34 +0800

    首页改版

--- src/main/resources/static/js/index/index.js
+++ src/main/resources/static/js/index/index.js
@@ -2,9 +2,22 @@
  * Created by lenovo on 2016-08-17.
  */
 // require(["module/name", ...], function(params){ ... });
-require(["jquery"], function ($) {
-    // Preloader
-    $('#status').delay(300).fadeOut();
-    $('#preloader').delay(300).fadeOut('slow');
-    $('body').delay(550).css({'overflow': 'visible'});
+require(["jquery", "bootstrap"], function ($) {
+    // qq social
+    $('.fa-qq').popover({
+        trigger: 'focus',
+        html: true,
+        animation: true,
+        content: '<img src="' + web_path + '/images/social-qq.png" style="height: 150px;width: 150px;" alt="QQ群" />',
+        placement: 'top'
+    });
+
+    // weixin social
+    $('.fa-weixin').popover({
+        trigger: 'focus',
+        html: true,
+        animation: true,
+        content: '<img src="' + web_path + '/images/social-weixin.jpg" style="height: 150px;width: 150px;" alt="微信公众号" />',
+        placement: 'top'
+    });
 });
