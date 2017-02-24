97af7bd28859d74bbb2c779da542a9c87151be93
zbeboy <863052317@qq.com>
2017-02-24 11:27:44 +0800

    系统日志记录切面完成

--- src/main/java/top/zbeboy/isy/aop/logging/LoggingAspect.java
+++ src/main/java/top/zbeboy/isy/aop/logging/LoggingAspect.java
@@ -10,9 +10,21 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
+import top.zbeboy.isy.annotation.logging.RecordSystemLogging;
 import top.zbeboy.isy.config.Workbook;
+import top.zbeboy.isy.domain.tables.pojos.SystemLog;
+import top.zbeboy.isy.domain.tables.pojos.Users;
+import top.zbeboy.isy.service.SystemLogService;
+import top.zbeboy.isy.service.UsersService;
+import top.zbeboy.isy.service.util.RequestUtils;
+import top.zbeboy.isy.service.util.UUIDUtils;
 
+import javax.annotation.Resource;
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.sql.Timestamp;
+import java.time.Clock;
 import java.util.Arrays;
 
 /**
@@ -31,8 +43,18 @@ public class LoggingAspect {
     @Inject
     private Environment env;
 
+    @Resource
+    private UsersService usersService;
+
+    @Resource
+    private SystemLogService systemLogService;
+
     @Pointcut("within(top.zbeboy.isy.service..*) || within(top.zbeboy.isy.web..*)")
     public void loggingPointcut() {
+    }
+
+    @Pointcut("@annotation(top.zbeboy.isy.annotation.logging.RecordSystemLogging)")
+    public void recordLoggingAspect() {
     }
 
     @AfterThrowing(pointcut = "loggingPointcut()", throwing = "e")
@@ -65,5 +87,33 @@ public class LoggingAspect {
 
             throw e;
         }
+    }
+
+    @Around("recordLoggingAspect()")
+    public Object doRecord(ProceedingJoinPoint point) throws Throwable {
+        String targetName = point.getTarget().getClass().getName();
+        String methodName = point.getSignature().getName();
+        Object[] arguments = point.getArgs();
+        Class targetClass = Class.forName(targetName);
+        Method[] methods = targetClass.getMethods();
+        for (Method method : methods) {
+            if (method.getName().equals(methodName)) {
+                Class[] clazzs = method.getParameterTypes();
+                if (clazzs.length == arguments.length) {
+                    for (Object o : arguments) {
+                        if (o instanceof HttpServletRequest) {
+                            HttpServletRequest request = (HttpServletRequest) o;
+                            Users users = usersService.getUserFromSession();
+                            SystemLog systemLog = new SystemLog(UUIDUtils.getUUID(), String.valueOf(method.getAnnotation(RecordSystemLogging.class).description()), new Timestamp(Clock.systemDefaultZone().millis()), users.getUsername(), RequestUtils.getIpAddress(request));
+                            systemLogService.save(systemLog);
+                            log.info(" Record operator logging to database , the module is {} , the method is {} ", method.getAnnotation(RecordSystemLogging.class).module(), method.getAnnotation(RecordSystemLogging.class).methods());
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+        return point.proceed();
     }
 }
