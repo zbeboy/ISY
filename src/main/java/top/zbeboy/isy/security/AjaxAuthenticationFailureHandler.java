7039f7ae7cf2d9b5de21a5d5396e32c3bae91589
zbeboy <863052317@qq.com>
2017-05-06 12:24:27 +0800

    明确登录日志

--- src/main/java/top/zbeboy/isy/security/AjaxAuthenticationFailureHandler.java
+++ src/main/java/top/zbeboy/isy/security/AjaxAuthenticationFailureHandler.java
@@ -1,13 +1,22 @@
 package top.zbeboy.isy.security;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+import top.zbeboy.isy.domain.tables.pojos.SystemLog;
+import top.zbeboy.isy.service.system.SystemLogService;
+import top.zbeboy.isy.service.util.RequestUtils;
+import top.zbeboy.isy.service.util.UUIDUtils;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.time.Clock;
 
 /**
  * Returns a 401 error code (Unauthorized) to the client, when Ajax authentication fails.
@@ -18,7 +27,13 @@ public class AjaxAuthenticationFailureHandler extends SimpleUrlAuthenticationFai
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
                                         AuthenticationException exception) throws IOException, ServletException {
-
+        ServletContext context = request.getSession().getServletContext();
+        ApplicationContext ctx = WebApplicationContextUtils
+                .getWebApplicationContext(context);
+        SystemLog systemLog = new SystemLog(UUIDUtils.getUUID(), "登录系统失败", new Timestamp(Clock.systemDefaultZone().millis()), request.getParameter("username"), RequestUtils.getIpAddress(request));
+        SystemLogService systemLogService = (SystemLogService) ctx
+                .getBean("systemLogService");
+        systemLogService.save(systemLog);
         response.getWriter().print(AjaxAuthenticationCode.AU_ERROR_CODE);
     }
 }
--- src/main/java/top/zbeboy/isy/security/AjaxAuthenticationSuccessHandler.java
+++ src/main/java/top/zbeboy/isy/security/AjaxAuthenticationSuccessHandler.java
@@ -2,14 +2,23 @@ package top.zbeboy.isy.security;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+import top.zbeboy.isy.domain.tables.pojos.SystemLog;
+import top.zbeboy.isy.service.system.SystemLogService;
+import top.zbeboy.isy.service.util.RequestUtils;
+import top.zbeboy.isy.service.util.UUIDUtils;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.time.Clock;
 
 /**
  * Spring Security success handler, specialized for Ajax requests.
@@ -23,7 +32,13 @@ public class AjaxAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuc
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication)
             throws IOException, ServletException {
-
+        ServletContext context = request.getSession().getServletContext();
+        ApplicationContext ctx = WebApplicationContextUtils
+                .getWebApplicationContext(context);
+        SystemLog systemLog = new SystemLog(UUIDUtils.getUUID(), "登录系统成功", new Timestamp(Clock.systemDefaultZone().millis()), request.getParameter("username"), RequestUtils.getIpAddress(request));
+        SystemLogService systemLogService = (SystemLogService) ctx
+                .getBean("systemLogService");
+        systemLogService.save(systemLog);
         response.getWriter().print(AjaxAuthenticationCode.OK_CODE);
     }
 }
