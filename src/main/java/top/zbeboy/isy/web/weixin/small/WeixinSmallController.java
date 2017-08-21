ee03a4fd863f50a83bf67de1d06b5ba8d89d07e5
zbeboy <863052317@qq.com>
2017-08-21 15:41:59 +0800

    微信小程序

---
+++ src/main/java/top/zbeboy/isy/web/weixin/small/WeixinSmallController.java
@@ -0,0 +1,38 @@
+package top.zbeboy.isy.web.weixin.small;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import top.zbeboy.isy.service.weixin.WeixinService;
+import top.zbeboy.isy.web.vo.weixin.WeixinVo;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by lenovo on 2017-08-21.
+ * 微信小程序接入
+ */
+@Slf4j
+@Controller
+public class WeixinSmallController {
+
+    @Resource
+    private WeixinService weixinService;
+
+    /**
+     * 微信接入检验
+     *
+     * @param weixinVo 参数
+     * @return 接入值
+     */
+    @RequestMapping(value = "/weixin/small/msg/words", method = RequestMethod.GET)
+    @ResponseBody
+    public String weixinValid(WeixinVo weixinVo) {
+        if (weixinService.checkSignature(weixinVo)) {
+            return weixinVo.getEchostr();
+        }
+        return "error";
+    }
+}
