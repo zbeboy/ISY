16f3e18030d0e228db57c6efe8c61a149399ac01
zbeboy <863052317@qq.com>
2017-10-31 23:51:02 +0800

    web socket config转kotlin

---
+++ src/main/java/top/zbeboy/isy/config/WebSocketConfiguration.kt
@@ -0,0 +1,31 @@
+package top.zbeboy.isy.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+
+/**
+ * Spring boot web socket.
+ *
+ * @author zbeboy
+ * @version 1.1
+ * @since 1.0
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+open class WebSocketConfiguration : AbstractWebSocketMessageBrokerConfigurer() {
+
+    override fun configureMessageBroker(config: MessageBrokerRegistry?) {
+        config!!.enableSimpleBroker("/topic")
+        config.setApplicationDestinationPrefixes("/app")
+    }
+
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        registry.addEndpoint("/remind").withSockJS()
+                .setStreamBytesLimit(512 * 1024)
+                .setHttpMessageCacheSize(1000)
+                .setDisconnectDelay(30 * 1_000L)
+    }
+}
