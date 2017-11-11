9106414b849da0add266465618570ce19c56ecbd
zbeboy <863052317@qq.com>
2017-11-11 13:29:24 +0800

    日志与邮件转kotlin

---
+++ src/main/java/top/zbeboy/isy/glue/system/SystemMailboxGlueImpl.kt
@@ -0,0 +1,138 @@
+package top.zbeboy.isy.glue.system
+
+import com.alibaba.fastjson.JSONObject
+import org.elasticsearch.index.query.QueryBuilder
+import org.elasticsearch.index.query.QueryBuilders
+import org.elasticsearch.search.sort.SortBuilders
+import org.elasticsearch.search.sort.SortOrder
+import org.springframework.data.domain.Page
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder
+import org.springframework.stereotype.Repository
+import org.springframework.util.ObjectUtils
+import org.springframework.util.StringUtils
+import top.zbeboy.isy.elastic.pojo.SystemMailboxElastic
+import top.zbeboy.isy.elastic.repository.SystemMailboxElasticRepository
+import top.zbeboy.isy.glue.plugin.ElasticPlugin
+import top.zbeboy.isy.glue.util.ResultUtils
+import top.zbeboy.isy.service.util.DateTimeUtils
+import top.zbeboy.isy.service.util.SQLQueryUtils
+import top.zbeboy.isy.web.bean.system.mailbox.SystemMailboxBean
+import top.zbeboy.isy.web.util.DataTablesUtils
+import java.util.*
+import javax.annotation.Resource
+
+/**
+ * Created by zbeboy 2017-11-11 .
+ **/
+@Repository("systemMailboxGlue")
+open class SystemMailboxGlueImpl : ElasticPlugin<SystemMailboxBean>(), SystemMailboxGlue {
+
+    @Resource
+    open lateinit var systemMailboxElasticRepository: SystemMailboxElasticRepository
+
+    override fun findAllByPage(dataTablesUtils: DataTablesUtils<SystemMailboxBean>): ResultUtils<List<SystemMailboxBean>> {
+        val search = dataTablesUtils.search
+        val resultUtils = ResultUtils<List<SystemMailboxBean>>()
+        val systemMailboxElasticPage = systemMailboxElasticRepository.search(buildSearchQuery(search, dataTablesUtils, false))
+        return resultUtils.data(dataBuilder(systemMailboxElasticPage)).totalElements(systemMailboxElasticPage.totalElements)
+    }
+
+    override fun countAll(): Long {
+        return systemMailboxElasticRepository.count()
+    }
+
+    override fun save(systemMailboxElastic: SystemMailboxElastic) {
+        systemMailboxElasticRepository.save(systemMailboxElastic)
+    }
+
+    /**
+     * 构建新数据
+     *
+     * @param systemMailboxElasticPage 分页数据
+     * @return 新数据
+     */
+    private fun dataBuilder(systemMailboxElasticPage: Page<SystemMailboxElastic>): List<SystemMailboxBean> {
+        val systemMailboxes = ArrayList<SystemMailboxBean>()
+        for (systemMailboxElastic in systemMailboxElasticPage.content) {
+            val systemMailboxBean = SystemMailboxBean()
+            systemMailboxBean.systemMailboxId = systemMailboxElastic.systemMailboxId
+            systemMailboxBean.sendTime = systemMailboxElastic.sendTime
+            systemMailboxBean.acceptMail = systemMailboxElastic.acceptMail
+            systemMailboxBean.sendCondition = systemMailboxElastic.sendCondition
+            val date = DateTimeUtils.timestampToDate(systemMailboxElastic.sendTime)
+            systemMailboxBean.sendTimeNew = DateTimeUtils.formatDate(date)
+            systemMailboxes.add(systemMailboxBean)
+        }
+        return systemMailboxes
+    }
+
+    /**
+     * 系统邮件全局搜索条件
+     *
+     * @param search 搜索参数
+     * @return 搜索条件
+     */
+    override fun searchCondition(search: JSONObject): QueryBuilder? {
+        val boolqueryBuilder = QueryBuilders.boolQuery()
+        if (!ObjectUtils.isEmpty(search)) {
+            val acceptMail = StringUtils.trimWhitespace(search.getString("acceptMail"))
+            if (StringUtils.hasLength(acceptMail)) {
+                val wildcardQueryBuilder = QueryBuilders.wildcardQuery("acceptMail", SQLQueryUtils.elasticLikeAllParam(acceptMail))
+                boolqueryBuilder.must(wildcardQueryBuilder)
+            }
+        }
+        return boolqueryBuilder
+    }
+
+    /**
+     * 系统邮件排序
+     *
+     * @param dataTablesUtils          datatables工具类
+     * @param nativeSearchQueryBuilder 查询器
+     */
+    override fun sortCondition(dataTablesUtils: DataTablesUtils<SystemMailboxBean>, nativeSearchQueryBuilder: NativeSearchQueryBuilder): NativeSearchQueryBuilder? {
+        val orderColumnName = dataTablesUtils.orderColumnName
+        val orderDir = dataTablesUtils.orderDir
+        val isAsc = "asc".equals(orderDir, ignoreCase = true)
+        if (StringUtils.hasLength(orderColumnName)) {
+            if ("system_mailbox_id".equals(orderColumnName, ignoreCase = true)) {
+                if (isAsc) {
+                    nativeSearchQueryBuilder.withSort(SortBuilders.fieldSort("systemMailboxId").order(SortOrder.ASC).unmappedType("string"))
+                } else {
+                    nativeSearchQueryBuilder.withSort(SortBuilders.fieldSort("systemMailboxId").order(SortOrder.DESC).unmappedType("string"))
+                }
+            }
+
+            if ("send_time".equals(orderColumnName, ignoreCase = true)) {
+                if (isAsc) {
+                    nativeSearchQueryBuilder.withSort(SortBuilders.fieldSort("sendTime").order(SortOrder.ASC).unmappedType("long"))
+                } else {
+                    nativeSearchQueryBuilder.withSort(SortBuilders.fieldSort("sendTime").order(SortOrder.DESC).unmappedType("long"))
+                }
+            }
+
+            if ("accept_mail".equals(orderColumnName, ignoreCase = true)) {
+                if (isAsc) {
+                    nativeSearchQueryBuilder.withSort(SortBuilders.fieldSort("acceptMail").order(SortOrder.ASC).unmappedType("string"))
+                    nativeSearchQueryBuilder.withSort(SortBuilders.fieldSort("systemMailboxId").order(SortOrder.ASC).unmappedType("string"))
+                } else {
+                    nativeSearchQueryBuilder.withSort(SortBuilders.fieldSort("acceptMail").order(SortOrder.DESC).unmappedType("string"))
+                    nativeSearchQueryBuilder.withSort(SortBuilders.fieldSort("systemMailboxId").order(SortOrder.DESC).unmappedType("string"))
+                }
+            }
+
+            if ("send_condition".equals(orderColumnName, ignoreCase = true)) {
+                if (isAsc) {
+                    nativeSearchQueryBuilder.withSort(SortBuilders.fieldSort("sendCondition").order(SortOrder.ASC).unmappedType("string"))
+                    nativeSearchQueryBuilder.withSort(SortBuilders.fieldSort("systemMailboxId").order(SortOrder.ASC).unmappedType("string"))
+                } else {
+                    nativeSearchQueryBuilder.withSort(SortBuilders.fieldSort("sendCondition").order(SortOrder.DESC).unmappedType("string"))
+                    nativeSearchQueryBuilder.withSort(SortBuilders.fieldSort("systemMailboxId").order(SortOrder.DESC).unmappedType("string"))
+                }
+            }
+
+        }
+        return nativeSearchQueryBuilder
+    }
+
+}
---
+++ src/main/java/top/zbeboy/isy/web/system/log/SystemLogController.kt
@@ -0,0 +1,56 @@
+package top.zbeboy.isy.web.system.log
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.ResponseBody
+import top.zbeboy.isy.glue.system.SystemLogGlue
+import top.zbeboy.isy.web.bean.system.log.SystemLogBean
+import top.zbeboy.isy.web.util.DataTablesUtils
+import java.util.ArrayList
+import javax.annotation.Resource
+import javax.servlet.http.HttpServletRequest
+
+/**
+ * Created by zbeboy 2017-11-11 .
+ **/
+@Controller
+@RequestMapping("/web")
+open class SystemLogController {
+
+    @Resource
+    open lateinit var systemLogGlue: SystemLogGlue
+
+    /**
+     * 系统日志
+     *
+     * @return 系统日志页面
+     */
+    @RequestMapping("/menu/system/log")
+    fun systemLog(): String {
+        return "web/system/log/system_log::#page-wrapper"
+    }
+
+    /**
+     * datatables ajax查询数据
+     *
+     * @param request 请求
+     * @return datatables数据
+     */
+    @RequestMapping(value = "/system/log/data", method = arrayOf(RequestMethod.GET))
+    @ResponseBody
+    fun systemLogs(request: HttpServletRequest): DataTablesUtils<SystemLogBean> {
+        // 前台数据标题 注：要和前台标题顺序一致，获取order用
+        val headers = ArrayList<String>()
+        headers.add("username")
+        headers.add("behavior")
+        headers.add("operating_time")
+        headers.add("ip_address")
+        val dataTablesUtils = DataTablesUtils<SystemLogBean>(request, headers)
+        val resultUtils = systemLogGlue.findAllByPage(dataTablesUtils)
+        dataTablesUtils.data = resultUtils.getData()
+        dataTablesUtils.setiTotalRecords(systemLogGlue.countAll())
+        dataTablesUtils.setiTotalDisplayRecords(resultUtils.getTotalElements())
+        return dataTablesUtils
+    }
+}
