7c452055bba3bab6155fc6a86617ea04acbbbc03
zbeboy <863052317@qq.com>
2017-11-20 10:55:37 +0800

    require js 转kotlin

---
+++ src/main/java/top/zbeboy/isy/web/webjars/RequireJSController.kt
@@ -0,0 +1,23 @@
+package top.zbeboy.isy.web.webjars
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.webjars.RequireJS
+import javax.servlet.http.HttpServletRequest
+
+/**
+ * 配置webjars与requireJS
+ *
+ * @author zbeboy
+ * @version 1.1
+ * @since 1.0
+ */
+@Controller
+open class RequireJSController {
+    @ResponseBody
+    @RequestMapping(value = "/webjarsjs", produces = arrayOf("application/javascript"))
+    fun webjarjs(request: HttpServletRequest): String {
+        return RequireJS.getSetupJavaScript(request.contextPath + "/webjars/")
+    }
+}
