4bbc78e39cdf6edb4bd9383ac7fbe7a71e6d1658
zbeboy <863052317@qq.com>
2018-03-19 11:18:54 +0800

    代码优化

--- src/main/java/top/zbeboy/isy/filter/SecurityLoginFilter.kt
+++ src/main/java/top/zbeboy/isy/filter/SecurityLoginFilter.kt
@@ -24,45 +24,53 @@ class SecurityLoginFilter : Filter {
             val password = StringUtils.trimWhitespace(request.getParameter("password"))
             val j_captcha_response = StringUtils.trimWhitespace(request.getParameter("j_captcha_response"))
 
-            if (StringUtils.hasLength(email)) {// 邮箱不为空
-                if (StringUtils.hasLength(password)) {// 密码不为空
-                    if (StringUtils.hasLength(j_captcha_response)) {// 验证码不为空
-                        if (CaptchaUtils.validCaptcha(j_captcha_response, request)) {// 检验验证码
-                            val context = request.session.servletContext
-                            val ctx = WebApplicationContextUtils
-                                    .getWebApplicationContext(context)
-                            val usersService = ctx!!
-                                    .getBean("usersService") as UsersService
-                            val users = usersService.findByUsername(email)
-                            if (!ObjectUtils.isEmpty(users)) {// 用户是否存在
-                                if (!ObjectUtils.isEmpty(users!!.enabled) && users.enabled > 0) {// 用户是否已被注销
-                                    if (!ObjectUtils.isEmpty(users.verifyMailbox) && users.verifyMailbox > 0) {// 用户邮箱是否已被验证
-                                        val isDel = usersService.validSCDSOIsDel(users)
-                                        if (!isDel) {// 用户所在院校是否已被注销
-                                            filterChain!!.doFilter(servletRequest, servletResponse)
-                                        } else {
-                                            response.writer.print(AjaxAuthenticationCode.SCHOOL_IS_DEL_CODE)
-                                        }
-                                    } else {
-                                        response.writer.print(AjaxAuthenticationCode.EMAIL_IS_NOT_VALID)
-                                    }
-                                } else {
-                                    response.writer.print(AjaxAuthenticationCode.USERNAME_IS_ENABLES)
-                                }
-                            } else {
-                                response.writer.print(AjaxAuthenticationCode.USERNAME_IS_NOT_EXIST_CODE)
-                            }
-                        } else {
-                            response.writer.print(AjaxAuthenticationCode.CAPTCHA_ERROR_CODE)
-                        }
-                    } else {
-                        response.writer.print(AjaxAuthenticationCode.CAPTCHA_IS_BLANK)
-                    }
-                } else {
-                    response.writer.print(AjaxAuthenticationCode.PASSWORD_IS_BLANK)
-                }
-            } else {
+            if (!StringUtils.hasLength(email)) {// 邮箱不为空
                 response.writer.print(AjaxAuthenticationCode.EMAIL_IS_BLANK)
+                return
+            }
+
+            if (!StringUtils.hasLength(password)) {// 密码不为空
+                response.writer.print(AjaxAuthenticationCode.PASSWORD_IS_BLANK)
+                return
+            }
+
+            if (!StringUtils.hasLength(j_captcha_response)) {// 验证码不为空
+                response.writer.print(AjaxAuthenticationCode.CAPTCHA_IS_BLANK)
+                return
+            }
+
+            if (!CaptchaUtils.validCaptcha(j_captcha_response, request)) {// 检验验证码
+                response.writer.print(AjaxAuthenticationCode.CAPTCHA_ERROR_CODE)
+                return
+            }
+
+            val context = request.session.servletContext
+            val ctx = WebApplicationContextUtils
+                    .getWebApplicationContext(context)
+            val usersService = ctx!!
+                    .getBean("usersService") as UsersService
+            val users = usersService.findByUsername(email)
+
+            if (ObjectUtils.isEmpty(users)) {// 用户是否存在
+                response.writer.print(AjaxAuthenticationCode.USERNAME_IS_NOT_EXIST_CODE)
+                return
+            }
+
+            if (ObjectUtils.isEmpty(users!!.enabled) || users.enabled <= 0) {// 用户是否已被注销
+                response.writer.print(AjaxAuthenticationCode.USERNAME_IS_ENABLES)
+                return
+            }
+
+            if (ObjectUtils.isEmpty(users.verifyMailbox) || users.verifyMailbox <= 0) {// 用户邮箱是否已被验证
+                response.writer.print(AjaxAuthenticationCode.EMAIL_IS_NOT_VALID)
+                return
+            }
+
+            val isDel = usersService.validSCDSOIsDel(users)
+            if (!isDel) {// 用户所在院校是否已被注销
+                filterChain!!.doFilter(servletRequest, servletResponse)
+            } else {
+                response.writer.print(AjaxAuthenticationCode.SCHOOL_IS_DEL_CODE)
             }
         } else {
             filterChain!!.doFilter(servletRequest, servletResponse)
