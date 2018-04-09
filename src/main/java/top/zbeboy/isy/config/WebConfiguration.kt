b3c2fbf924aa9a6b5d634f392bd2a4c8182389ee
zhaoyin <863052317@qq.com>
2018-04-09 10:13:14 +0800

    首页设置

--- src/main/java/top/zbeboy/isy/config/WebConfiguration.kt
+++ src/main/java/top/zbeboy/isy/config/WebConfiguration.kt
@@ -43,6 +43,7 @@ open class WebConfiguration : WebMvcConfigurer {
     /**
      * 设置默认首页
      * 注: 这里会指定该url为最高级，不会被spring security 影响到，否则会跳转到login页面.
+     * spring 默认支持首页index.html，这里添加是因为可以对首页做一些数据显示操作.
      */
     override fun addViewControllers(registry: ViewControllerRegistry?) {
 
