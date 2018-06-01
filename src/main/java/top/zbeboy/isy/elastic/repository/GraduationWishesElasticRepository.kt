cf8f2fac292528ae235420a80d33f711d805dfdc
zbeboy <863052317@qq.com>
2018-06-01 10:39:01 +0800

    1.3 start.

--- src/main/java/top/zbeboy/isy/elastic/repository/GraduationWishesElasticRepository.kt
+++
@@ -1,9 +0,0 @@
-package top.zbeboy.isy.elastic.repository
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
-import top.zbeboy.isy.elastic.pojo.GraduationWishesElastic
-
-/**
- * Created by zbeboy 2018-05-23 .
- **/
-interface GraduationWishesElasticRepository : ElasticsearchRepository<GraduationWishesElastic, String>
